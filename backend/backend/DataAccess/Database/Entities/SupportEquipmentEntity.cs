cc157bb6201129adb8c8890881d3cde907648067
Themba <=>
2020-06-26 23:05:01 +0200

    added new static data for database

--- backend/backend/DataAccess/Database/Entities/SupportEquipmentEntity.cs
+++ backend/backend/DataAccess/Database/Entities/SupportEquipmentEntity.cs
@@ -13,5 +13,7 @@ namespace backend.DataAccess.Database.Entities
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int id { get; set; }
         public string name { get; set; }
+        public double time_study_per_test { get; set; }
+        public double rate_per_hour { get; set; }
     }
 }
