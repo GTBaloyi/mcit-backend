7d574aa1c796e20232da81b65c96f90784ee4d06
Themba <=>
2020-08-21 14:45:24 +0200

    alter user service

--- backend/backend/Migrations/20200821102350_mcts-db-update-v1.6.Designer.cs
+++ backend/backend/Migrations/20200821102350_mcts-db-update-v1.6.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using backend.DataAccess.Repositories;
 
 namespace backend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200821102350_mcts-db-update-v1.6")]
+    partial class mctsdbupdatev16
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
