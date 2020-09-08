e3cdcc5dbc75f8d07d063e37bc05cadfd0ff59d0
Themba <=>
2020-09-08 20:16:44 +0200

    field and database changes

--- backend/backend/Migrations/20200908181530_mcts-db-v2.3.Designer.cs
+++ backend/backend/Migrations/20200908181530_mcts-db-v2.3.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using backend.DataAccess.Repositories;
 
 namespace backend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200908181530_mcts-db-v2.3")]
+    partial class mctsdbv23
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
